f1ae5c8ce854132b0bfeed2c1d8f9f98e614b290
MarcSeemann <m.seemann@posteo.de>
2024-10-11 11:59:05 +0200

    Correct source placement

--- source/geometries/Cigar.cc
+++ source/geometries/Cigar.cc
@@ -110,7 +110,7 @@ namespace nexus {
     // inside_cigar_ = new BoxPointSampler(1*mm, 1*mm, 1*mm, 0, G4ThreeVector(0.,0.,0.));
     G4RotationMatrix *y_rot_180 = new G4RotationMatrix();
     y_rot_180->rotateY(0 * deg);
-    inside_cigar_ = new BoxPointSampler(1*mm, 1*mm, 1*mm, 0, G4ThreeVector(0, chamber_diameter+3*mm +3*cm, 0), y_rot_180);
+    inside_cigar_ = new BoxPointSampler(1*mm, 1*mm, 1*mm, 0, G4ThreeVector(0, chamber_diameter+3*mm, 0), y_rot_180);
 
     world_z_ = cigar_length_ * 20;
     world_xy_ = cigar_width_ * 20;
