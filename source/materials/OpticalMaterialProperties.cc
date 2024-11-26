d72941e5fd2d8a13e81b24106683afa7a09dbfed
MarcSeemann <m.seemann@posteo.de>
2024-11-26 12:11:47 +0100

    Correct PTFE reflectivity

--- source/materials/OpticalMaterialProperties.cc
+++ source/materials/OpticalMaterialProperties.cc
@@ -781,8 +781,8 @@ namespace opticalprops {
     //   .72,  .72,  .72
     // };
     std::vector<G4double> REFLECTIVITY = {
-      .03,  .03,  .03,  0.03,
-      .03,  .03,  .03
+      .98,  .98,  .98,  0.98,
+      .72,  .03,  .03
     };
     mpt->AddProperty("REFLECTIVITY", ENERGIES, REFLECTIVITY);
 
